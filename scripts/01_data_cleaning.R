c3ce31f51856d933166454d4e27df4bdec02ba7d
Lizbeth Ramirez <lizbetkr@uci.edu>
2026-03-11 01:10:45 -0700

    Full cleaning script

--- scripts/01_data_cleaning.R
+++ scripts/01_data_cleaning.R
@@ -1,9 +1,32 @@
 # import necessary libraries
 library(tidyverse)
+library(dplyr)
 
 # load music data set
 music <- read_csv("data/music.csv")
 
-# check data
-glimpse(music)
+# drop completely unknown columns as specified in the data set documentation
+music <- music %>%
+  select(-artist.location, -artist.similar, -release.name, -song.artist_mbtags)
 
+# replace placeholder values (0 -> NA)
+# columns with 0 as unknown: song.year and song.hotttnesss
+music <- music %>% 
+  mutate(
+    song.year = ifelse(song.year == 0, NA, song.year),
+    song.hotttnesss = ifelse(song.hotttnesss < 0, NA, song.hotttnesss)
+    )
+
+# convert columns to categories
+music <- music %>%
+  mutate(
+    artist.id = factor(artist.id),
+    artist.name = factor(artist.name),
+    song.id = factor(song.id)
+  )
+
+# check missing values
+colSums(is.na(music))
+
+# save cleaned data set
+write_csv(music, "data/music_clean.csv")
